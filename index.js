2838ab490f58c48c475c9a425affa31d52f909da
ryanholly3 <ryanholly3@gmail.com>
2018-10-17 11:31:16 -0600

    added body-parser

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const bodyparser = require('body-parser')
 
 app.use(cors());
+app.user(bodyparser());
 
 app.get('/', (req, res, next) =>{
   res.send('DERP')
